69c3f80e232fb3d471ec40940fbf64069017cfb4
Dante Fragapane <dante.fragapane@gmail.com>
2020-05-26 15:47:17 -0400

    Finished the help command

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+module.exports = {
+    help: {
+        name: "Help",
+        description: "The... the help command. Really? You wanted to know more about the help command?"
+    },
+    run: (bot, message, args) => {
+        console.log(args)
+        const command = bot.commands.get(args[0])
+        if(!command) {
+            message.reply("Please give a valid command.") 
+            return  false
+        }
+        message.reply(command.help.description)
+    }
+}
